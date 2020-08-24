f388c654a26d5482758f68c49fa906247495433c
unknown <liiban333@gmail.com>
2020-08-23 20:19:34 -0500

    added routes

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static('public'));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.use(express.static("public"));
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+    console.log("App listening on PORT: " + PORT);
 });
