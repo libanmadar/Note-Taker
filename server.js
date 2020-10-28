ad4bfa6d06839ee70cd19aeb8d27e3219fee7b9a
unknown <liiban333@gmail.com>
2020-10-27 22:54:56 -0500

    refactored express server

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 const express = require("express");
-const path = require("path");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
